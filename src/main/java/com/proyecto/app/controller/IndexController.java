aaf32f6b82a12c9c84e273a3660a6485b3a94f98
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-10-14 18:36:52 -0300

    inicio

---
+++ src/main/java/com/proyecto/app/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.proyecto.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String index() {
+        return "forms/index";
+    }
+    
+}
---
+++ src/main/java/com/proyecto/app/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.proyecto.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "forms/login";
+    }
+    
+}
