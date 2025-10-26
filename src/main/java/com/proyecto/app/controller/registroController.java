26cd266fa1d05e80bf1ec5265a38edc4b778a462
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-10-26 12:14:46 -0300

    detalles

---
+++ src/main/java/com/proyecto/app/controller/registroController.java
@@ -0,0 +1,12 @@
+package com.proyecto.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class registroController {
+    @GetMapping("/registro")
+    public String registro(){
+        return "registro";
+    }
+}
