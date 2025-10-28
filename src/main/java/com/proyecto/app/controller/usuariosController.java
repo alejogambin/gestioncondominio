a6678a825a1a3476855996f6b6922b28f14ca6af
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-10-28 00:09:02 -0300

    vistas

--- src/main/java/com/proyecto/app/controller/usuariosController.java
+++ src/main/java/com/proyecto/app/controller/usuariosController.java
@@ -2,11 +2,19 @@ package com.proyecto.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @Controller
 public class usuariosController {
     @GetMapping("/usuarios")
     public String usuarios() {
         return "usuario/Usuarios";
+    }
+    @PostMapping("path")
+    public String postMethodName(@RequestBody String entity) {
+        
+        return entity;
     }
     
 }
