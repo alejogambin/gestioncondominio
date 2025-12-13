927ac3ad47dfffcc56a3ff680548ecbc25919a25
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-12-12 21:04:04 -0300

    Securityconfig archivo refactorizado

--- src/main/java/com/proyecto/config/SecurityConfig.java
+++ src/main/java/com/proyecto/config/SecurityConfig.java
@@ -1,80 +1,87 @@
-
 package com.proyecto.config;
-
-import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.proyecto.app.entity.Usuario;
-import com.proyecto.app.service.UsuarioService;
 import com.proyecto.app.service.impl.UsuarioServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http
-		.authorizeHttpRequests(auth -> auth
-			// recursos estaticos publicos
-			.requestMatchers("/css/**", "/js/**","/images/**")
-			.permitAll()
+    private final UsuarioServiceImpl userDetailsService;
 
-			// paginas publicas
-			.requestMatchers("/login", "/registro", "/index", "/contacto")
-			.permitAll()
-
-			//rutas protegidas por rol
-			.requestMatchers("/dasboard/**").hasRole("ADMIN")
-
-			.requestMatchers("/dasboard").hasRole("USER")
-
-			//todo lo demas requiere autentificacion
-			.anyRequest().authenticated()
-		)
-			.formLogin(login -> login 
-				.loginPage("/login.html")
-				.loginProcessingUrl("/login")
-				.defaultSuccessUrl("/dasboard", true)
-				.usernameParameter("correo")
-				.passwordParameter("contraseña")
-				.failureUrl("/login?error=true")
-				.permitAll()			
-			)
-
-			.logout(logout -> logout
-				.logoutUrl("/logout")
-				.permitAll()
-			)
-
-		.build();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	/*
-    @Bean
-    public UserDetailsService userDEtailsService() throws Exception{
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        List<Usuario> usuarios = UsuarioServiceImpl.findAll();
-        for(Usuario usuario : usuarios){
-            manager.createUser(org.springframework.security.core.userdetails.User
-            .withUsername(usuario.getNombre())
-            .password(passwordEncoder().encode(usuario.getContraseña()))
-            .roles("USER").build());
-        }
-        return manager;
+    public SecurityConfig(UsuarioServiceImpl userDetailsService ){
+        this.userDetailsService = userDetailsService;
     }
-		 */
-	 
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/css/**",
+            "/js/**",
+            "/img/**",
+            "/webjars/**",
+            "/h2-console/**",
+            "/",
+            "/index",
+            "/home",
+            "/inicio",
+            "/registro")
+            .permitAll()
+
+            .requestMatchers("/usuario/**").hasAnyAuthority("ADMIN")
+
+            .anyRequest().authenticated()
+        )
+    
+        .formLogin(login -> login
+            .loginPage("/login")
+            .loginProcessingUrl("/login")
+            .usernameParameter("email")
+            .passwordParameter("contraseña")
+            .defaultSuccessUrl("/dasbord",true)
+            .failureUrl("/login?error=true")
+            .permitAll()
+        )
+
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+
+            .logoutSuccessUrl("/login?logout=true")
+
+            .invalidateHttpSession(true)
+
+            .deleteCookies("JSESSIONID")
+
+            .permitAll()
+
+        )
+        //.csrf().disable()
+        .build();
+    }
 }
